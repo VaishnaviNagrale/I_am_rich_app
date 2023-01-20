f8e1b3cff8996bbb59314b40ae3b6230b5e08f3e
Vaishnavi Nagrale <Vaishnavinagrale2@gmail.com>
2023-01-20 12:26:07 +0530

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+//App start to run from here
+void main() {
+  runApp(
+    MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Center(
+          child: Text('💰I Am Rich💰'),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image:
+              // NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPTYz6zMmmk92pD_-XM5XPhseOP3QsglcqGg&usqp=CAU'),
+              AssetImage('assests/Images/lightning.jpg.jpg'),
+          ),
+      ),
+    ),
+  ),
+  );
+}
